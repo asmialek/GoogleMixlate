d801e7b7f91e408030fecc0e4c2e9ad5a8484bad
asmialek <asmialek@cbk.waw.pl>
2018-08-10 13:01:12 +0100

    Final CBK commit

---
+++ docs/build/html/_sources/index.rst.txt
@@ -0,0 +1,23 @@
+.. Mixlate documentation master file, created by
+   sphinx-quickstart on Thu Jan  4 13:21:21 2018.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Mixlate's documentation!
+===================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+.. automodule:: mixlate
+   :members:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- mixlate.py
+++ mixlate.py
@@ -1,78 +1,74 @@
 from googletrans import Translator
+import progressbar
 import random
 
-"""
-Language Code
--------- ----
-Afrikaans 	af
-Albanian 	sq
-Arabic 	    ar
-Belarusian 	be
-Bulgarian 	bg
-Catalan 	ca
-Chinese Simplified 	    zh-CN
-Chinese Traditional 	zh-TW
-Croatian 	hr
-Czech 	    cs
-Danish 	    da
-Dutch 	    nl
-English 	en
-Estonian 	et
-Filipino 	tl
-Finnish 	fi
-French 	fr
-Galician 	gl
-German 	de
-Greek 	el
-Hebrew 	iw
-Hindi 	hi
-Hungarian 	hu
-Icelandic 	is
-Indonesian 	id
-Irish 	ga
-Italian 	it
-Japanese 	ja
-Korean 	ko
-Latvian 	lv
-Lithuanian 	lt
-Macedonian 	mk
-Malay 	ms
-Maltese 	mt
-Norwegian 	no
-Persian 	fa
-Polish 	pl
-Portuguese 	pt
-Romanian 	ro
-Russian 	ru
-Serbian 	sr
-Slovak 	sk
-Slovenian 	sl
-Spanish 	es
-Swahili 	sw
-Swedish 	sv
-Thai 	th
-Turkish 	tr
-Ukrainian 	uk
-Vietnamese 	vi
-Welsh 	cy
-Yiddish 	yi
-"""
-
-# translator = Translator()
-#
-# text = translator.translate('Hello there, general!', dest='pl')
-# print(text)
+languages = {'Afrikaans': 'af',
+             'Albanian': 'sq',
+             'Arabic': 'ar',
+             'Belarusian': 'be',
+             'Bulgarian': 'bg',
+             'Catalan': 'ca',
+             'Chinese Simplified': 'zh-CN',
+             'Chinese Traditional': 'zh-TW',
+             'Croatian': 'hr',
+             'Czech': 'cs',
+             'Danish': 'da',
+             'Dutch': 'nl',
+             'English': 'en',
+             'Estonian': 'et',
+             'Filipino': 'tl',
+             'Finnish': 'fi',
+             'French': 'fr',
+             'Galician': 'gl',
+             'German': 'de',
+             'Greek': 'el',
+             'Hebrew': 'iw',
+             'Hindi': 'hi',
+             'Hungarian': 'hu',
+             'Icelandic': 'is',
+             'Indonesian': 'id',
+             'Irish': 'ga',
+             'Italian': 'it',
+             'Japanese': 'ja',
+             'Korean': 'ko',
+             'Latvian': 'lv',
+             'Lithuanian': 'lt',
+             'Macedonian': 'mk',
+             'Malay': 'ms',
+             'Maltese': 'mt',
+             'Norwegian': 'no',
+             'Persian': 'fa',
+             'Polish': 'pl',
+             'Portuguese': 'pt',
+             'Romanian': 'ro',
+             'Russian': 'ru',
+             'Serbian': 'sr',
+             'Slovak': 'sk',
+             'Slovenian': 'sl',
+             'Spanish': 'es',
+             'Swahili': 'sw',
+             'Swedish': 'sv',
+             'Thai': 'th',
+             'Turkish': 'tr',
+             'Ukrainian': 'uk',
+             'Vietnamese': 'vi',
+             'Welsh': 'cy',
+             'Yiddish': 'yi'}
 
 
 def mixup(phrase='Hello World!', steps=5, my_src=None, my_dest=None):
-    translator = Translator()
+    """Hello!
 
-    languages = ["af", "sq", "ar", "be", "bg", "ca", "zh-CN", "zh-TW", "hr",
-                 "cs", "da", "nl", "en", "et", "tl", "fi", "fr", "gl", "de",
-                 "el", "iw", "hi", "hu", "is", "id", "ga", "it", "ja", "ko",
-                 "lv", "lt", "mk", "ms", "mt", "no", "fa", "pl", "pt", "ro",
-                 "ru", "sr", "sk", "sl", "es", "sw", "sv", "th", "tr", "uk",
-                 "vi", "cy", "yi"]
+    Args:
+        phrase (str): Lol
+        steps:
+        my_src:
+        my_dest:
+
+    Returns:
+
+    """
+    translator = Translator()
 
     path = []
 
@@ -87,17 +83,25 @@ def mixup(phrase='Hello World!', steps=5, my_src=None, my_dest=None):
 
     temp_text = phrase
 
-    for i in range(steps):
-        temp_dest = random.choice(languages)
-        path.append(temp_dest)
+    bar = progressbar.ProgressBar()
+
+    for i in bar(range(steps)):
+        temp_key = random.choice(list(languages.keys()))
+        temp_dest = languages[temp_key]
+        path.append(translator.translate(temp_key, src='en', dest=my_dest).text.capitalize())
         temp_text = translator.translate(temp_text, src=temp_src, dest=temp_dest)
         # print(temp_text)
         temp_text = temp_text.text
         temp_src = temp_dest
 
-    print(path)
-    return translator.translate(temp_text, src=temp_src, dest=my_dest)
+    return translator.translate(temp_text, src=temp_src, dest=my_dest).text, path
 
-text = 'Merry christmas and a happy new year!'
-print(text)
-print(mixup(phrase=text, steps=20).text)
+if __name__ == '__main__':
+    text = 'Wesołych Świąt i szczęśliwego Nowego Roku!'
+    translation, path = mixup(phrase=text, my_src='pl', steps=25)
+    print(text)
+    i = 0
+    for item in path:
+        i += 1
+        print(item + '   ', end='')
+    print('\n' + translation)
